f538b53d48a5fd804f7086655ac6d07543767323
Swati Agarwal <swati.lmgclko@gmail.com>
2016-07-17 19:28:24 +0530

    removed media files

--- internship/urls.py
+++ internship/urls.py
@@ -26,7 +26,7 @@ from django.contrib.auth.views import logout
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'', login_required(views.karigars_all())),
+    url(r'', views.login_user),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', logout, kwargs={'next_page': 'login'}, name='logout'),
