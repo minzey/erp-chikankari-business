ba208ed644b777ca5c36cde22ba1e3c5e394399d
Swati Agarwal <swati.lmgclko@gmail.com>
2016-08-01 22:04:30 +0530

    redirect from setting

--- internship/settings/production.py
+++ internship/settings/production.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 import os
 import dj_database_url
-
+from django.http import HttpResponseRedirect
 
 DEBUG = True
 TEMPLATE_DEBUG = True
@@ -25,21 +25,4 @@ try:
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 except:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-
-    DATABASES = settings.DATABASES
-    DATABASES['default'].update(db_from_env)
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    HttpResponseRedirect('../../karigars_all/')
