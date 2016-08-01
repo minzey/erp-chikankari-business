2d5eeb295f54f89beb4bef5dc2e7d04a95092349
Swati Agarwal <swati.lmgclko@gmail.com>
2016-08-01 22:26:07 +0530

    persistent conn 5 sec

--- internship/settings/production.py
+++ internship/settings/production.py
@@ -5,24 +5,15 @@ from django.http import HttpResponseRedirect
 
 DEBUG = True
 TEMPLATE_DEBUG = True
-try:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-
-    DATABASES = settings.DATABASES
-    DATABASES['default'].update(db_from_env)
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-except:
-    HttpResponseRedirect('../../karigars_all/')
+db_from_env = dj_database_url.config(conn_max_age=5)
+DATABASES = settings.DATABASES
+DATABASES['default'].update(db_from_env)
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+# Static files (CSS, JavaScript, Images)
+#  https://docs.djangoproject.com/en/1.8/howto/static-files/
+#  Simplified static file serving.
+#  https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
