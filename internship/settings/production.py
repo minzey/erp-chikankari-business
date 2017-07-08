2bcbb30cba77a06bffe2d40d0ecaac12017a540b
Swati Agarwal <swati.lmgclko@gmail.com>
2017-07-08 13:25:13 +0530

    debug false

--- internship/settings/production.py
+++ internship/settings/production.py
@@ -4,8 +4,8 @@ import dj_database_url
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.http import HttpResponseRedirect
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 db_from_env = dj_database_url.config(conn_max_age=5)
 DATABASES = settings.DATABASES
 DATABASES['default'].update(db_from_env)
@@ -18,13 +18,8 @@ ALLOWED_HOSTS = ['*']
 #  Simplified static file serving.
 #  https://warehouse.python.org/project/whitenoise/
 
-AWS_ACCESS_KEY_ID = "AKIAJBZOY2CWTBOQLKXQ"
-AWS_SECRET_ACCESS_KEY = "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"
-# MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
-S3_BUCKET = 'taanabaana'
-AWS_STORAGE_BUCKET_NAME = 'taanabaana'
 S3DIRECT_REGION = 'ap-southeast-1'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
