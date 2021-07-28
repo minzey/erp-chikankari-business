9eb76401586a2cc29934bc119760e52218052f56
Swati Agarwal <swati.lmgclko@gmail.com>
2021-07-28 20:32:56 +0530

    Making wsgi code compatible with whitenoise version 5.3

--- internship/wsgi.py
+++ internship/wsgi.py
@@ -17,12 +17,6 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "internship.settings")
 application = get_wsgi_application()
-
-try:
-    application = DjangoWhiteNoise(application)
-except:
-    pass
